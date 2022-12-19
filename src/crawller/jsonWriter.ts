4d4ccc3fa2600443c0652ca90c7bcae5c2085efd
LiteHell <litehell@litehell.info>
2022-12-19 23:48:26 +0900

    Write JSON in more efficient way

--- src/crawller/jsonWriter.ts
+++ src/crawller/jsonWriter.ts
@@ -9,33 +9,43 @@ type JsonResultItem = {
 
 export default class JsonWriter implements FileWriter {
     _file!: fs.WriteStream;
-    _result: JsonResultItem[] = [];
+    _firstItem: boolean = true;
     async prepare (filename: string, option: FileWriterPrepareOption){
         this._file = fs.createWriteStream(filename, {encoding: 'utf8'});
+        this._file.write('[');
     }
     async write(campus: Campus, college: CauCollege, department?: CauDepartment, subject?: CauSubject): Promise<void> {
+        let item;
+        if (!this._firstItem) {
+            this._file.write(',');
+        }
         if (subject && department) {
-            this._result.push({
+            item = {
                 campus: campus == Campus.Seoul ? '서울' : '안성',
                 subject,
                 department,
                 college
-            })
+            };
         } else if (department) {
-            this._result.push({
+            item = {
                 campus: campus == Campus.Seoul ? '서울' : '안성',
                 department,
                 college
-            })
+            };
         } else {
-            this._result.push({
+            item = {
                 campus: campus == Campus.Seoul ? '서울' : '안성',
                 college
-            })
+            };
         }
+
+        if (this._firstItem)
+            this._firstItem = false;
+
+        this._file.write(JSON.stringify(item));
     }
     async finish(): Promise<void> {
-        this._file.write(JSON.stringify(this._result));
+        this._file.write(']');
         this._file.end();
     }
 }
